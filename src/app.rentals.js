b617f5d0085c1fce75d7d9ae17fd045609a07c80
ralph yozzo <ralph@brooklynmarathon.com>
2024-07-04 10:38:08 -0400

    ralph-desktop

--- src/app.rentals.js
+++ src/app.rentals.js
@@ -35,7 +35,7 @@ const typesenseInstantsearchAdapter = new TypesenseInstantSearchAdapter({
     // queryBy: 'title,authors',
     // queryBy: 'data.PropAddr,data.PropOwner',
     // queryBy: "data.searchkey, data.id, data.BillYear, data.PropAddr, data.PropAssessed, data.PropOwes, data.PropOwner, data.description, data.eventid",
-    query_by: "snippet,loc,lastmod",
+    query_by: "filter,snippet,loc,lastmod",
   },
 });
 const searchClient = typesenseInstantsearchAdapter.searchClient;
